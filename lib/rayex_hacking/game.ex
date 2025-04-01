8037e60b22eb8d5ecdc7b4554ddc550fb5df13d6
Dan Bowles <daniel.bowles@gmail.com>
2025-04-01 14:15:14 -0400

    :sparkles: Screens!

--- lib/rayex_hacking/game.ex
+++ lib/rayex_hacking/game.ex
@@ -5,17 +5,22 @@ defmodule RayexHacking.Game do
 
   # Config
   @fps 60
+  @key_enter 257
   @width 800
   @height 450
   @title "Rayex Hacking"
+  @logo_timeout 180
   # Colors
+  @color_white %RS.Color{r: 255, g: 255, b: 255, a: 255}
   @color_green_a %RS.Color{r: 0, g: 100, b: 48, a: 126}
   @color_gray %RS.Color{r: 0, g: 0, b: 0, a: 255}
 
   defmodule State do
     defstruct ball_pos: {400, 225},
               ball_speed: {4, 5},
-              ball_radius: 20.0
+              ball_radius: 20.0,
+              game_screen: :logo,
+              frame_count: 0
   end
 
   def start_link(_opts) do
@@ -24,7 +29,6 @@ defmodule RayexHacking.Game do
 
   @impl true
   def init(:ok) do
-    IO.inspect("Starting Rayex Hacking Application")
     init_window(@width, @height, @title)
     set_target_fps(@fps)
 
@@ -39,23 +43,69 @@ defmodule RayexHacking.Game do
       System.stop(0)
       {:stop, :normal, state}
     else
-      new_state = update_game(state)
-
       begin_drawing()
-      clear_background(@color_green_a)
 
-      {x, y} = new_state.ball_pos
-      draw_circle(trunc(x), trunc(y), new_state.ball_radius, @color_gray)
-      draw_text("Rayex Hacking", 10, 10, 20, @color_gray)
-      draw_fps(10, @height - 24)
+      render_screen(state)
+      next_state = update_game(state)
 
       end_drawing()
-      tick(new_state)
+
+      # Tick Frame Count
+      next_state = %{next_state | frame_count: next_state.frame_count + 1}
+      # Tick with new state
+      tick(next_state)
     end
   end
 
+  defp render_screen(%State{game_screen: :logo, frame_count: frame_count})
+       when frame_count <= @logo_timeout do
+    clear_background(@color_white)
+    draw_text("LOGO SCREEN", 10, 10, 20, @color_gray)
+    draw_text("WAIT for 3 SECONDS...", 290, 220, 20, @color_gray)
+  end
+
+  defp render_screen(%State{game_screen: :title}) do
+    clear_background(@color_white)
+    draw_text("TITLE SCREEN", 10, 10, 20, @color_gray)
+    draw_text("SKIP with [ENTER]", 290, 220, 20, @color_gray)
+  end
+
+  defp render_screen(%State{game_screen: :game} = state) do
+    clear_background(@color_green_a)
+
+    {x, y} = state.ball_pos
+    draw_circle(trunc(x), trunc(y), state.ball_radius, @color_gray)
+    draw_text("Rayex Hacking", 10, 10, 20, @color_gray)
+    draw_fps(10, @height - 24)
+  end
+
+  defp render_screen(%State{game_screen: :credits}) do
+    IO.inspect("Rendering Credits Screen")
+  end
+
   # Game Logic
-  defp update_game(state) do
+  defp update_game(%{game_screen: :logo} = state) do
+    # Update the game screen to title after the logo timeout
+    if state.frame_count >= @logo_timeout do
+      %{state | game_screen: :title, frame_count: 0}
+    else
+      state
+    end
+  end
+
+  defp update_game(%{game_screen: :title} = state) do
+    case is_key_pressed?(@key_enter) do
+      true ->
+        # Update the game screen to game
+        %{state | game_screen: :game}
+
+      false ->
+        # Do nothing, stay on title screen
+        state
+    end
+  end
+
+  defp update_game(%{game_screen: :game} = state) do
     {x, y} = state.ball_pos
     {dx, dy} = state.ball_speed
 
